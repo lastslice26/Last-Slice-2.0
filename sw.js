2f2a59e4852f99a213a148cd6f42368e8e8730b6
lastslice26 <last.slice@gmx.de>
2026-06-29 10:12:54 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE="last-slice-os-7";const FILES=["./","./index.html","./manifest.webmanifest"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
